bea8ce0607725c650014a9eba84537b2b4f7c650
rvaldenegrof <r.valdenegrof@gmail.com>
2021-08-01 14:08:23 -0400

    Finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <Todo :title="'Lista del supermercado'" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from '@/components/Todo.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Todo,
   },
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <li>
+      <div v-if="!tarea.modificable">{{ tarea.$index + 1 }}: {{ tarea.texto }} <button @click="borrar(tarea)">Eliminar</button></div>
+      <!--<input v-if="tarea.modificable" type="text" v-model="tarea.texto" />
+      <button @click="modificar(tarea)">M</button> -->
+  </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  props: {
+    tarea: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    borrar(tarea) {
+      this.$emit('borrar-tarea', tarea)
+    },
+  },
+}
+</script>
+
+<style></style>
